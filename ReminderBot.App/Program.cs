f9dae345399f0874e9eb794bd89a5e1514f38d06
Steven Garberg <steven.garberg.jr@gmail.com>
2022-07-28 20:32:26 -0400

    Setup bot console app

---
+++ ReminderBot.App/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ReminderBot.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunBotAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static DiscordSocketClient _client;
+        private static CommandService _commands;
+        private static IServiceProvider _services;
+
+        public static async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            const string token = "REPLACE_ME";
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+        
+        private static async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private static async Task HandleCommandAsync(SocketMessage socketMessage)
+        {
+            var message = socketMessage as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("reminderbot ", ref argPos))
+            {
+                await Execute();
+                return;
+            }
+            argPos = 0;
+            if (message.HasStringPrefix("<@897551401369100358> ", ref argPos))
+            {
+                await Execute();
+                return;
+            }
+
+            async Task Execute()
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
