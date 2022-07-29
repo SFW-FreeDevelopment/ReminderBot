b4703714299724d7a2a1b7ed4dcddf6df4ac9263
Steven Garberg <steven.garberg.jr@gmail.com>
2022-07-29 19:45:27 -0400

    Added basic commands

---
+++ ReminderBot.App/Commands/HelpCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ReminderBot.App.Commands
+{
+    public class HelpCommand : CommandBase
+    {
+        [Command("help")]
+        public async Task HandleCommandAsync()
+        {
+            await ReplyAsync($"**The following commands can be used:**{Environment.NewLine}" +
+                             $"  • **ping** - Pings the Discord channel{Environment.NewLine}" +
+                             $"  • **konami** - Displays the Konami code as emojis{Environment.NewLine}");
+        }
+    }
+}
