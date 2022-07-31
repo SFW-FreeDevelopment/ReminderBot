f837675019078c25dca639942d0c03a3c493e0d8
Steven Garberg <steven.garberg.jr@gmail.com>
2022-07-31 16:23:58 -0400

    Updated model

---
+++ ReminderBot.App/Models/BaseResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ReminderBot.App.Models;
+
+public abstract class BaseResource
+{
+    [Required, BsonId] public string Id { get; set; }
+    [Required] public DateTime CreatedAt { get; set; }
+    [Required] public DateTime UpdatedAt { get; set; }
+    [Required, ConcurrencyCheck] public int Version { get; set; }
+
+    protected BaseResource()
+    {
+        Id = Guid.NewGuid().ToString();
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+        Version = 1;
+    }
+}
--- ReminderBot.App/Models/Reminder.cs
+++ ReminderBot.App/Models/Reminder.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using Discord.Commands;
-using Discord.WebSocket;
 
 namespace ReminderBot.App.Models
 {
-    public class Reminder
+    public class Reminder : BaseResource
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
         public string Message { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime RemindAt { get; set; }
         public bool HasTriggered { get; set; }
         public SocketCommandContext Context { get; set; }
